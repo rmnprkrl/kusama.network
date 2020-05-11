994ccf4b6cc1d070ce1f333f023ff5375a751874
Max S <hello@thisisvilla.com>
2020-05-11 19:02:54 +0200

    initial cleanup / codekit setup

---
+++ assets/pages/home/home.js
@@ -0,0 +1,7 @@
+// import $ from 'jquery'
+// // export for others scripts to use
+// window.$ = $
+
+window.addEventListener('load', function() {
+	document.querySelector('body').classList.add('loaded')
+})
