ed59fedcccf4cef345375428506ec29746bdbba5
Max S <hello@thisisvilla.com>
2020-05-11 21:32:39 +0200

    hero fixes

--- assets/pages/home/home.js
+++ assets/pages/home/home.js
@@ -1,5 +1,20 @@
+import Typed from 'typed.js'
+
 window.addEventListener('load', init)
+
+const typedOpts = {
+	strings: [
+		'Expect chaos.',
+		'No promises.',
+		'Polkadot&#39;s wild cousin.',
+		'A canary network.',
+		'A Polkadot experiment.'
+	],
+	typeSpeed: 100
+}
 
 function init() {
 	document.querySelector('body').classList.add('loaded')
+
+	new Typed('.typed-text', typedOpts)
 }
