3e502488dc86539f6032148fffeb9de838fa83af
Max S <hello@thisisvilla.com>
2020-05-15 03:35:42 +0200

    animationsss

--- assets/pages/home/home.js
+++ assets/pages/home/home.js
@@ -19,18 +19,22 @@ const projectOpts = {
 	loop: true
 }
 
+export const mapRange = (value, x1, y1, x2, y2) => (value - x1) * (y2 - x2) / (y1 - x1) + x2
+
 const handleScroll = () => {
 	let scrollTop = window.scrollY
 
 	window.requestAnimationFrame(() => {
 		heroScene(scrollTop)
+		boxScene()
+		kusamaScene()
+		launchScene()
+		parallax()
 	})
 }
 
 // ------------ SCENES
 const heroScene = scrollPos => {
-	if (scrollPos > 200) return
-
 	const el = document.getElementById('hero-scene')
 
 	if (scrollPos < 150) {
@@ -40,11 +44,99 @@ const heroScene = scrollPos => {
 	}
 }
 
+const boxScene = () => {
+	const el = document.getElementById('box-scene')
+	const move = el.querySelector('.move')
+	let windowHeight = window.innerHeight
+	let top = el.getBoundingClientRect().top - windowHeight
+	let bottom = el.getBoundingClientRect().bottom
+	let height = el.getBoundingClientRect().height
+	let duration = height + windowHeight
+
+	if (top <= 0 && bottom >= 0) {
+		let progress = top * -1
+		progress = progress / duration
+
+		progress = progress * 100
+
+		let output = mapRange(progress, 0, 100, -50, 50)
+
+		move.style.transform = `translateY(${output}%)`
+	}
+}
+
+const kusamaScene = () => {
+	const el = document.getElementById('kusama-scene')
+
+	let windowHeight = window.innerHeight
+	let top = el.getBoundingClientRect().top - windowHeight
+	let bottom = el.getBoundingClientRect().bottom
+	let height = el.getBoundingClientRect().height
+	let duration = height + windowHeight
+
+	if (top <= 0 && bottom >= 0) {
+		let progress = top * -1
+		progress = progress / duration
+
+		progress = progress * 100
+
+		let output = mapRange(progress, 0, 100, 0, 5)
+
+		el.style.setProperty('--progress', output + '%')
+	}
+}
+
+const launchScene = () => {
+	const el = document.getElementById('launch-scene')
+	const move = el.querySelector('.move')
+	let windowHeight = window.innerHeight
+	let top = el.getBoundingClientRect().top - windowHeight
+	let bottom = el.getBoundingClientRect().bottom
+	let height = el.getBoundingClientRect().height
+	let duration = height + windowHeight
+
+	if (top <= 0 && bottom >= 0) {
+		let progress = top * -1
+		progress = progress / duration
+
+		progress = progress * 100
+
+		let output = mapRange(progress, 0, 100, -10, 30)
+
+		move.style.transform = `translateY(${output}%)`
+	}
+}
+
+let parallaxEls
+
 function init() {
 	new Typed('.typed-text-hero', heroOpts)
 	new Typed('.typed-text-project', projectOpts)
 
+	parallaxEls = document.querySelectorAll('.parallax')
+
 	window.addEventListener('scroll', handleScroll)
+}
+
+const parallax = () => {
+	let windowHeight = window.innerHeight
+	parallaxEls.forEach(el => {
+		let top = el.getBoundingClientRect().top - windowHeight
+		let bottom = el.getBoundingClientRect().bottom
+		let height = el.getBoundingClientRect().height
+		let duration = height + windowHeight
+
+		if (top <= 0 && bottom >= 0) {
+			let progress = top * -1
+			progress = progress / duration
+
+			progress = progress * 100
+
+			// let output = mapRange(progress, 0, 100, -10, 30)
+
+			el.style.setProperty('--progress', Math.floor(progress) + '%')
+		}
+	})
 }
 
 window.addEventListener('load', init)
